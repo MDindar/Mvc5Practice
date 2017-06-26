69c40a7eecf6d197838553e3abab46512aac1cd1
Mostafa Dindar <dindar@asus.com>
2017-06-26 21:43:12 +0430

    passing viewModel to the view

--- MVC5Practice/Controllers/MoviesController.cs
+++ MVC5Practice/Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using MVC5Practice.Models;
+using MVC5Practice.ViewModels;
 
 namespace MVC5Practice.Controllers
 {
@@ -13,14 +14,26 @@ namespace MVC5Practice.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie() {Id = 1, Name = "movie1"};
+            var movie = new Movie() { Id = 1, Name = "movie1" };
             //return View(movie);
-            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+            // return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+           // var viewResult = new ViewResult();
+           var customers = new List<Customer>()
+           {
+               new Customer(){Name = "customer1"},
+               new Customer(){Name = "customer2"}
+           };
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
 
         }
 
         [Route("Movies/News/{year:regex(\\d{4}):range(2000,2018)}/{month:regex(\\d{2})}")]
-        public ActionResult NewsByDate(int year , int month)
+        public ActionResult NewsByDate(int year, int month)
         {
             return Content($"Year={year}&Month={month}");
         }
---
+++ MVC5Practice/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC5Practice.Models;
+
+namespace MVC5Practice.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
