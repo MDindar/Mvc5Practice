35c51c75033e0f32f1a33dc2c96e18be2bfc43c1
Mostafa Dindar <dindar@asus.com>
2017-06-30 14:31:24 +0430

    adding publish practice

---
+++ MVC5Practice/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Practice.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Error
+        public ActionResult NotFound()
+        {
+            ViewBag.TabTitle = "Page Not Found";
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
